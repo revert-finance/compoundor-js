41a446233020889cd611e29d9bc27f6d3b9bed4c
kalinbas <kalinbas@gmail.com>
2023-01-31 11:30:19 -0600

    fixed promise handling

--- index.js
+++ index.js
@@ -367,7 +367,7 @@ async function getGasPrice(isEstimation) {
 
 async function waitWithTimeout(tx, timeoutMs) {
     return new Promise((resolve, reject) => {
-        tx.wait(1).then(() => resolve())
+        tx.wait(1).then(() => resolve()).catch(err => reject(err))
         setTimeout(() => resolve(), timeoutMs)
     })
 }
